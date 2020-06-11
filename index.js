c878d65151b849f016d4b1d41e1125b7b72dfba8
Juan Oyarzún <juan.oyarzun.gajardo@hotmail.com>
2020-06-11 11:31:02 -0400

    Implementación de lint y se mueven los schemas a un archivo propio

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 'use strict'
 
-const { graphql, buildSchema} = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const graphMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
 const app = express()
 const port = process.env.port || 3000
 
-//Defined schema
-const schema = buildSchema(`
-    type Query {
-    hello: String,
-    saludo: String
-}`)
+// Reading schema
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
 
-//Resolvers
+// Resolvers
 const resolvers = {
-    hello: () => { return 'hello world!'},
-    saludo: () => { return 'hello juan!'},
+  hello: () => { return 'hello world!' },
+  saludo: () => { return 'hello juan!' }
 }
 
-//Execute query
-graphql(schema, '{ hello, saludo}', resolvers).then(data => console.log(data))
+// Execute query
+// graphql(schema, '{ hello, saludo}', resolvers).then(data => console.log(data))
 
 app.use('/api', graphMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}/api`))
