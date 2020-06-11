48358dbfe8be723a72f945410f2d5ebba7063938
Juan Oyarzún <juan.oyarzun.gajardo@hotmail.com>
2020-06-11 11:36:55 -0400

    Se mueven los resolvers a un archivo propio

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Development server
 
-Run `npm i` for install dependencies
-Run `npm run start` for a dev server. Navigate to `http://localhost:3000/api`. The app will automatically reload if you change any of the source files.
+Run `npm i` for install dependencies\
+Run `npm run start` for a dev server. Navigate to `http://localhost:3000/api`. The app will automatically reload if you change any of the source files.\
 
--- index.js
+++ index.js
@@ -5,18 +5,13 @@ const express = require('express')
 const graphMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // Reading schema
 const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
-
-// Resolvers
-const resolvers = {
-  hello: () => { return 'hello world!' },
-  saludo: () => { return 'hello juan!' }
-}
 
 // Execute query
 // graphql(schema, '{ hello, saludo}', resolvers).then(data => console.log(data))
